5bf011d0a55a117ee746502e586e83fd125f0ded
gunturputprat <gppratama17@gmail.com>
2020-02-14 11:40:25 +0700

    help and feedback

--- Containers/HelpFeedbackScreen.js
+++ Containers/HelpFeedbackScreen.js
@@ -7,26 +7,162 @@ import {
   View,
   Dimensions,
   TextInput,
+  ScrollView,
   TouchableOpacity,
 } from 'react-native';
 import {Col, Row, Grid} from 'react-native-easy-grid';
-import {Dropdown} from 'react-native-material-dropdown';
-
+import DropDownItem from 'react-native-drop-down-item';
 export default class HelpFeedbackScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contents: [
+        {
+          title: '1. Apa itu Bieon Salt Smart Detector?' + '\n',
+          body:
+            '• Bieon Salt Smart Detector adalah alat ukur digital untuk mengukur kualitas garam dengan hasil pengukuran realtime',
+        },
+        {
+          title:
+            '2. Parameter apa saja yang dapat diukur dengan menggunakan Bieon Salt Smart Detector?' +
+            '\n',
+          body:
+            '• Bieon Salt Smart Detector dapat digunakan untuk mengukur Kadar NaCl, derajat putih (whiteness) dan Kadar Air (water content) garam yang masih berbentuk kristal.' +
+            '\n' +
+            '\n' +
+            '• Range pengukuran Bieon Salt Smart Detector yaitu :' +
+            '\n' +
+            'NaCl = 80% – 99.99%' +
+            '\n' +
+            'Whiteness = 45% – 95%' +
+            '\n' +
+            'Kadar Air   = 0.5% – 12%',
+        },
+        {
+          title: '3. Apa saja keunggulan Bieon Salt Smart Detector?' + '\n',
+          body:
+            '• Dapat mengukur 3 parameter kualitas garam dengan waktu yang bersamaan' +
+            '\n' +
+            '\n' +
+            '• Hasil pengukuran yang akurat' +
+            '\n' +
+            '\n' +
+            '• Hasil pengukuran dapat langsung dicetak (print)' +
+            '\n' +
+            '\n' +
+            '• Ukuran yang kecil dan portable' +
+            '\n' +
+            '\n' +
+            '• Sudah tersertifikasi oleh syscal' +
+            '\n' +
+            '\n' +
+            '• Menggunakan baterai yang dapat diisi ulang',
+        },
+        {
+          title: '4. Apakah Bieon Salt Smart Detector Ready Stock?' + '\n',
+          body:
+            '• Pembelian Bieon Salt Smart Detector indent dengan waktu maksimal 45 hari kerja setelah PO diterima',
+        },
+        {
+          title: '5. Berapa harga Bieon Salt Smart Detector?' + '\n',
+          body: '• Silahkan hubungi contact center : +6282320007800',
+        },
+        {
+          title: '6. Apakah ada garansi untuk barang yang dibeli?' + '\n',
+          body:
+            '• Garansi Spare Part : 1 bulan' +
+            '\n' +
+            '• Garansi Service : 6 bulan',
+        },
+        {
+          title:
+            '7.Apabila pada saat penggunaan, ditemui kendala. Apakah yang harus dilakukan?' +
+            '\n',
+          body: '• Silahkan hubungi contact center : +6282320007800',
+        },
+        {
+          title: '8. Mengapa struk/kertas hasil print tidak keluar?' + '\n',
+          body:
+            '• Hasil pengukuran dapat langsung dicetak (print) dengan menekan tombol “PRINT”' +
+            'pada alat. Bila setelah ditekan tombol “PRINT” tapi tidak keluar kertas, dapat' +
+            'dikarenakan kertas printer telah habis. Silahkan melakukan pengisian ulang (refill)' +
+            'kertas printer. Cara mengisi kertas printer dapat dilihat pada video berikut.',
+        },
+        {
+          title:
+            '9. Kapan Bieon Salt Smart Detector harus dicharge / isi ulang baterai?' +
+            '\n',
+          body:
+            '• Status baterai Bieon Salt Smart Detector bisa dilihat dilihat melalui aplikasi pada menu “Device info” dan bisa di charge melalui wireless charger.',
+        },
+        {
+          title:
+            '10. Kapan Bieon Salt Smart Detector harus dikalibrasi?' + '\n',
+          body:
+            '• Bieon Salt Smart Detector disarankan untuk dikalibrasi setiap 1 tahun sekali.',
+        },
+      ],
+    };
+  }
   render() {
-    let data = [
-      {
-        value: 'Banana',
-      },
-      {
-        value: 'Mango',
-      },
-      {
-        value: 'Pear',
-      },
-    ];
-
-    return <Dropdown label="Favorite Fruit" data={data} />;
+    const {navigate} = this.props.navigation;
+    return (
+      <Grid>
+        <Row size={13}>
+          <View style={styles.container}>
+            <View style={styles.topheader}>
+              <Image
+                style={styles.gear}
+                source={require('../assets/icons/setting/help-tr.png')}
+              />
+              <Text style={styles.titlesetting}>Help & Feedback</Text>
+              {/* <Image
+            style={styles.icontop}
+            source={require('../assets/logo/settingwhite.png')}
+          /> */}
+            </View>
+            <ScrollView
+              style={{alignSelf: 'stretch', marginTop: 180, marginLeft: 10}}>
+              {this.state.contents
+                ? this.state.contents.map((param, i) => {
+                    return (
+                      <DropDownItem
+                        key={i}
+                        style={styles.dropDownItem}
+                        contentVisible={false}
+                        invisibleImage={'x'}
+                        visibleImage={'-'}
+                        header={
+                          <View>
+                            <Text
+                              style={{
+                                fontSize: 18,
+                                color: 'black',
+                                fontWeight: 'bold',
+                              }}>
+                              {param.title}
+                            </Text>
+                          </View>
+                        }>
+                        <Text
+                          style={[
+                            styles.txt,
+                            {
+                              fontSize: 18,
+                            },
+                          ]}>
+                          {param.body}
+                        </Text>
+                      </DropDownItem>
+                    );
+                  })
+                : null}
+              <View style={{height: 96}} />
+            </ScrollView>
+          </View>
+        </Row>
+      </Grid>
+    );
   }
 }
 
@@ -48,95 +184,26 @@ const styles = StyleSheet.create({
     height: 40,
     marginTop: -25,
   },
-  ColLeft: {
-    height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: -50,
+  topheader: {
+    height: 150,
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    position: 'absolute',
+    top: 0,
+    backgroundColor: '#129cd8',
+    width: 420,
   },
-  ColRight: {
-    height: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: -50,
+  gear: {
+    width: 120,
+    height: 120,
   },
-  background: {
-    width: win.width,
-    height: win.height,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: -5,
-  },
-  text: {
-    color: '#fff',
-    fontSize: 10,
-    marginTop: 50,
-    color: '#f8f8f8',
-  },
-  textsign: {
-    color: '#fff',
-    fontSize: 13,
-    marginTop: 60,
-  },
-  itemContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-    alignSelf: 'stretch',
-  },
-  itemIconImage: {
-    resizeMode: 'contain',
-    width: 30,
-    height: 30,
-  },
-  IconImage: {
-    resizeMode: 'contain',
-    width: 30,
-    height: 30,
-  },
-  TextInput: {
-    fontSize: 18,
-    alignSelf: 'stretch',
-    width: 330,
-    height: 40,
-    marginBottom: 20,
-    color: '#000',
-    borderBottomColor: '#000',
-    borderBottomWidth: 0.7,
-    fontStyle: 'italic',
-    marginLeft: 15,
-  },
-  button: {
-    alignSelf: 'center',
-    alignItems: 'center',
-    borderRadius: 40,
-    width: 230,
-    height: 50,
-    padding: 10,
-    backgroundColor: '#fff',
-    marginTop: 40,
-  },
-  buttonGoogle: {
-    alignSelf: 'center',
-    alignItems: 'center',
-    borderRadius: 40,
-    width: 230,
-    height: 50,
-    padding: 10,
-    backgroundColor: 'red',
-    marginTop: 20,
-  },
-  textbutton: {
-    fontSize: 20,
-    color: '#129cd8',
-    fontWeight: '700',
-    textAlign: 'center',
-  },
-  textbuttonGoogle: {
-    fontSize: 20,
-    color: '#fff',
-    fontWeight: '700',
-    textAlign: 'center',
-    marginLeft: 20,
+  titlesetting: {
+    fontSize: 25,
+    color: '#ffffff',
+    top: 20,
+    left: 20,
+    position: 'absolute',
+    textAlign: 'justify',
+    borderStyle: 'solid',
   },
 });
